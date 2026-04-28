7587cb8c30ce23b76bfcd5a1bd9259d6cf745f04
btnav <174347826+bt-nav@users.noreply.github.com>
2026-04-28 17:30:38 +0100

    Support for config and launch folders

--- src/rover/setup.py
+++ src/rover/setup.py
@@ -1,30 +1,40 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
-package_name = 'rover'
+package_name = "rover"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.yaml")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Team 4',
-    maintainer_email='174347826+bt-nav@users.noreply.github.com',
-    description='This package should be installed on the Leo Rover.',
-    license='BSD-3-Clause',
+    maintainer="Team 4",
+    maintainer_email="174347826+bt-nav@users.noreply.github.com",
+    description="This package should be installed on the Leo Rover.",
+    license="BSD-3-Clause",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'rover_example_node = rover.rover_example_node:main'
+        "console_scripts": [
+            "rover_example_node = rover.rover_example_node:main",
         ],
     },
 )
